976e30a1ad628cb8485631742a892fdd45de3c38
Mpiafe <mpiaferreyra@gmail.com>
2023-05-05 07:43:19 -0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import NavBar from './components/nav/NavBar';
-import Home from "./components/Home/Home";
+import Home from "./components/home/Home";
 import { CartProvider } from './context/CartContext';
 import CartContent from './components/cart/CartContent';
 
